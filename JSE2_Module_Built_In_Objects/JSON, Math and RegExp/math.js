d5934fdc758a23ed1034f367188c2944ccfdf6f0
David Padilla <david.padillac15@gmail.com>
2024-11-29 06:20:19 -0600

    Create math.js
    Adding build in methods for math

---
+++ JSE2_Module_Built_In_Objects/JSON, Math and RegExp/math.js
@@ -0,0 +1,28 @@
+console.log("--------Math-------------");
+console.log(Math.PI); // -> 3.141592653589793
+console.log(Math.E); // -> 2.718281828459045
+
+
+console.log("--------Rounding-------------");
+//The ceil method rounds the given number up to the first integer greater than the one given in the argument.
+
+//The floor method rounds down to the first integer less than the one given.
+
+//The round method rounds the figure according to the rules of arithmetic (up if the decimal fraction is equal to or greater than 0.5, down if not).
+
+console.log(Math.ceil(10.2)); // -> 11
+console.log(Math.floor(10.2)); // -> 10
+console.log(Math.round(10.2)); // -> 10
+console.log(Math.ceil(10.499999)); // -> 11 
+console.log(Math.floor(10.499999)); // -> 10
+console.log(Math.round(10.499999)); // -> 10
+console.log(Math.ceil(10.5)); // -> 11
+console.log(Math.floor(10.5)); // -> 10
+console.log(Math.round(10.5)); // -> 11
+console.log(Math.ceil(10.8)); // -> 11
+console.log(Math.floor(10.8)); // -> 10
+console.log(Math.round(10.8)); // -> 11
+
+
+
+console.log("--------Drawing numbers-------------");
